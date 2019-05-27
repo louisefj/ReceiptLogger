846b31056433cbf8ed62483102be3f25cd6d209c
louisefj <l1jensen@ucsd.edu>
2019-05-26 21:04:18 -0700

    Add files via upload

---
+++ Receipt Logger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Receipt Logger
+//
+//  Created by Louise on 5/26/19.
+//  Copyright © 2019 louise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
